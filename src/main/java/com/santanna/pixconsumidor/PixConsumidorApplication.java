ec642493462c4fb949292586be9d96088dde259d
LSantAnnaB <l.sant.anna.b@gmail.com>
2024-04-09 09:52:39 -0300

    Stat Project

---
+++ src/main/java/com/santanna/pixconsumidor/PixConsumidorApplication.java
@@ -0,0 +1,13 @@
+package com.santanna.pixconsumidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixConsumidorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixConsumidorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/santanna/pixconsumidor/PixConsumidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santanna.pixconsumidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixConsumidorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
